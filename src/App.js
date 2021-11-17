a2c9642831d1914bc305829f30682db870aef038
LeonidMakhov <makhov-leonid999@yandex.ru>
2021-11-17 12:19:13 +0300

    create reorder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
+
+const initialList = [
+    {id: 1, name: '1  Create APP'},
+    {id: 2, name: '2  Learn JS'},
+    {id: 3, name: '3  Create IoS App'}, //up 3 instead 2
+    {id: 4, name: '4  Create Web App'},
+    {id: 5, name: '5  Learn React'},
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [list, setList] = useState(initialList);
+
+    const up = (id) => {
+        const index = list.findIndex(el => el.id === id);
+        console.log('Up', id, index);
+        const res = [...list.slice(0, index-1), list[index], list[index - 1],
+            ...list.slice(index + 1)]
+        setList(res);
+
+    };
+
+    const down = (id) => {
+        const index = list.findIndex(el => el.id === id);
+        console.log('Down', id, index);
+        const res = [...list.slice(0, index), list[index + 1], list[index],
+            ...list.slice(index + 2)]
+        setList(res);
+    };
+
+
+    return (
+        <div className="App">
+
+            {list.map((el, index) => (
+                <div key={el.id}>
+                    {index !== 0 ? <button onClick={() => up(el.id)}>Up</button> : null}
+                    {index !== list.length - 1 ? <button onClick={() => down(el.id)}>Down</button> : null}
+                    {el.name}
+                </div>))}
+
+        </div>
+    );
 }
 
 export default App;
